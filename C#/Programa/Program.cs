570786776498b26d0b00b981bd28d1e337a2d548
SFedora <s13flores18@gmail.com>
2026-05-03 23:50:50 -0400

    Primer aprendisaje C#
    Estamos recien aprendiendo C# por lo que estabamos realizando un mini programa donde podemos aprender mediante la ejecución de codigo

---
+++ C#/Programa/Program.cs
@@ -0,0 +1,19 @@
+﻿// Codigo basico enn C#
+
+// class Introducción // Es el cuerpo del codigo
+// {
+//   static void Main() // Es el iniciador del codigo
+//   {
+//     string saludo = "\nHola, Mundo!"; // Para asignar variables usas string
+//     Console.WriteLine(saludo); // Imprimir por consola variable saludo
+//     Console.Write("\nEscribe tu nombre: "); // Imprime en consola
+//     string? nombre = Console.ReadLine(); // Asigna una variable y pregunta al usuario que quiere q contenga
+//     Console.WriteLine($"\nTu nombre es {nombre}\n"); // Imprime todo pero con la variable nombre
+//   }
+// }
+//
+
+class variables
+{
+    static void Main() { }
+}
